1fe1b9c73c55fcd69df22f980139ee941a78d18d
Mikaels132 <miroruuska045@gmail.com>
2025-11-16 15:06:38 +0200

    EE

--- src/main/java/me/Mikzyy/tutorialLib/TutorialLib.java
+++ src/main/java/me/Mikzyy/tutorialLib/TutorialLib.java
@@ -4,6 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TutorialLib extends JavaPlugin {
 
+    private static TutorialLib instance;
+
+    private final JavaPlugin plugin;
+
+    public TutorialLib(JavaPlugin plugin) {
+        instance = this;
+        this.plugin = plugin;
+    }
+
+    public static TutorialLib getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -13,5 +26,9 @@ public final class TutorialLib extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public JavaPlugin getPlugin() {
+        return plugin;
     }
 }
